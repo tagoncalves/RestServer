f40235afd245b16db0fd56ca97f93f9087288132
Tagoncalves <tomasalejo2008@hotmail.com>
2021-11-24 13:48:07 -0300

    Cifrado de contraseña 0.1

--- database/config.js
+++ database/config.js
@@ -3,16 +3,15 @@ const mongoose = require('mongoose');
 const dbConection = async() => {
 
     try {
-        
+        // ORM para MongoDB
         await mongoose.connect( process.env.MONGODB_CNN );
 
-        console.log('Base de datos Online');
+        console.log('Data Base Online');
 
     } catch (error) {
         console.log(error);
         throw new Error('Error a la hora de iniciar la Base de Datos');
     }
-
 
 }
 
