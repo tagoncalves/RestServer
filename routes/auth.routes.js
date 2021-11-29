26dc15f1050b0ca74752ff9d8d201b38d67cb472
Tagoncalves <tomasalejo2008@hotmail.com>
2021-11-29 17:00:58 -0300

    ruta login declarada en Auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,16 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { login } = require('../controllers/auth');
+const { fieldsValidate } = require('../middlewares/fieldsValidate');
 
 const router = Router();
 
 
-router.post('/login', login);
-
-
+router.post('/login',[
+    check('correo', 'The  email is requested').isEmail(),
+    check('correo', 'The password is requested').not().isEmpty(),
+    fieldsValidate
+] ,login );
 
 
 
